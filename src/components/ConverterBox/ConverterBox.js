450fdee66e4dca62d120e1c91eba198bebdc0095
Rafael-Fontes-Baptista <rafaelfontesb@gmail.com>
2021-06-16 09:50:33 +0100

    Add currency list state by fetching data from the API

--- src/components/ConverterBox/ConverterBox.js
+++ src/components/ConverterBox/ConverterBox.js
@@ -1,6 +1,31 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 import "./ConverterBox.css"
 
-export default function ConverterBox() {
+export default function ConverterCard() {
+  const [currencyList, set_currencyList] = useState([])
+
+  useEffect(() => {
+    const fetchRatesList = async () => {
+      try {
+        const API_key = process.env.REACT_APP_EXCHANGE_RATES_API_KEY
+        const API_url = `http://api.exchangeratesapi.io/v1/latest?access_key=${API_key}`
+
+        const response = await axios.get(API_url)
+
+        const currencies = Object.keys(response.data.rates)
+        const currenciesAndRate = currencies.map((key) => ({
+          acronym: key,
+          rate: response.data.rates[key],
+        }))
+
+        set_currencyList(currenciesAndRate)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchRatesList()
+  }, [])
+
   return <div className="converter-box"></div>
 }
