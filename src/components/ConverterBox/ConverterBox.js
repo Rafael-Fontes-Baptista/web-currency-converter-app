4576c852ad32af2e3b346ced60cf6fdbc581c869
Rafael-Fontes-Baptista <rafaelfontesb@gmail.com>
2021-06-16 10:19:44 +0100

    Create ConverterTitle component

--- src/components/ConverterBox/ConverterBox.js
+++ src/components/ConverterBox/ConverterBox.js
@@ -1,31 +1,36 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios"
+import ConverterTitle from "./ConverterTitle/ConverterTitle"
 import "./ConverterBox.css"
 
-export default function ConverterCard() {
-  const [currencyList, set_currencyList] = useState([])
+export default function ConverterBox() {
+  // const [currencyList, set_currencyList] = useState([])
 
-  useEffect(() => {
-    const fetchRatesList = async () => {
-      try {
-        const API_key = process.env.REACT_APP_EXCHANGE_RATES_API_KEY
-        const API_url = `http://api.exchangeratesapi.io/v1/latest?access_key=${API_key}`
+  // useEffect(() => {
+  //   const fetchRatesList = async () => {
+  //     try {
+  //       const API_key = process.env.REACT_APP_EXCHANGE_RATES_API_KEY
+  //       const API_url = `http://api.exchangeratesapi.io/v1/latest?access_key=${API_key}`
 
-        const response = await axios.get(API_url)
+  //       const response = await axios.get(API_url)
 
-        const currencies = Object.keys(response.data.rates)
-        const currenciesAndRate = currencies.map((key) => ({
-          acronym: key,
-          rate: response.data.rates[key],
-        }))
+  //       const currencies = Object.keys(response.data.rates)
+  //       const currenciesAndRate = currencies.map((key) => ({
+  //         acronym: key,
+  //         rate: response.data.rates[key],
+  //       }))
 
-        set_currencyList(currenciesAndRate)
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    fetchRatesList()
-  }, [])
+  //       set_currencyList(currenciesAndRate)
+  //     } catch (error) {
+  //       console.log(error)
+  //     }
+  //   }
+  //   fetchRatesList()
+  // }, [])
 
-  return <div className="converter-box"></div>
+  return (
+    <div className="converter-box">
+      <ConverterTitle />
+    </div>
+  )
 }
